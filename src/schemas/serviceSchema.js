89faf83dc32b0eb9b60fd73efb825d48953df048
Renato Rosa Franco <renato_ny@live.com>
2024-09-14 13:11:38 -0300

    feature: updating project compoments and .env.example.

---
+++ src/schemas/serviceSchema.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export const initialValues = {
+    title: '',
+    description: '',
+    price: '',
+    category: ''
+}
+
+export const productSchema = Yup.object().shape({
+    title: Yup.string()
+        .required('O título é obrigatório'),
+    description: Yup.string()
+        .required('A descrição é obrigatória'),
+    price: Yup.number()
+        .required('O preço é obrigatório'),
+    category: Yup.string()
+        .required('A categoria é obrigatória')
+});
