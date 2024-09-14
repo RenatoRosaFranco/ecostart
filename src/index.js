3f8e0e9b3b03095ec325275a34bfbd1b0222ed5c
Renato Rosa Franco <renato_ny@live.com>
2024-09-14 18:19:48 -0300

    feature: updating Index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import rollbar from "./config/rollbar";
 import App from './App';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+window.addEventListener('error', (event) => {
+    rollbar.error(event.error);
+});
+
+window.addEventListener('unhandledrejection', (event) => {
+    rollbar.error(event.reason);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
