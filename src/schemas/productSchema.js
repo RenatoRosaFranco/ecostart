770145ebc581d59d573170be0fd9d2f67ae5890e
Renato Rosa Franco <renato_ny@live.com>
2024-09-14 22:39:45 -0300

    feature: added product schema and review business.

---
+++ src/schemas/productSchema.js
@@ -0,0 +1,31 @@
+import * as Yup from 'yup';
+
+export const initialValues = {
+    name: '',
+    description: '',
+    price: '',
+    category: '',
+    stock: ''
+}
+
+export const productSchema = Yup.object().shape({
+    name: Yup.string()
+        .min('O nome deve ter pelo menos 2 caracteres')
+        .max('O nome pode ter no máximo 50 caracteres')
+        .required('O nome é obrigatório'),
+    description: Yup.string()
+        .max(500, 'A descrição pode ter no máximo 500 caracteres')
+        .required('A descrição é obrigatória'),
+    price: Yup.number()
+        .typeError('O preço deve ser um número')
+        .positive('O preço deve ser um valor positivo')
+        .required('O preço é obrigatório'),
+    category: Yup.string()
+        .oneOf([], 'Categoria inválida')
+        .required('A categoria é obrigatória'),
+    stock: Yup.number()
+        .typeError('O estoque deve ser um número')
+        .integer('O estoque deve ser um número inteiro')
+        .min(0, 'O estoque não pode ser negativo')
+        .required('O estoque é obrigatório')
+});
